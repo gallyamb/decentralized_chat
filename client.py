5e0fe041f0b2899d856efd5261ce820436041d70
Gallyam <gallyamb@gmail.com>
2015-01-09 12:06:15 +0500

    Добавлена передачай файлов, список клиентов, приват

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+import os
+import random
+import subprocess
+import sys
+
 __author__ = 'Галлям'
 
 import select
@@ -5,6 +10,7 @@ import socket
 import json
 import logging
 import threading
+import time
 
 from PyQt5 import QtCore
 
@@ -37,7 +43,7 @@ class ClientInfo:
 
     def __eq__(self, other):
         return self.name == other.name and \
-               self.port == other.port
+            self.port == other.port
 
     def __ne__(self, other):
         return not self == other
@@ -54,12 +60,16 @@ class DataContainer:
 
 class Client(QtCore.QObject):
     new_message = QtCore.pyqtSignal(str)
+    new_client = QtCore.pyqtSignal(str)
+    client_deleted = QtCore.pyqtSignal(str)
+    upload_request = QtCore.pyqtSignal(str, str, str)  # filename, size, client name
+    busy = QtCore.pyqtSignal()
 
     def __init__(self, port: int, name: str):
         super().__init__()
         logging.basicConfig(filename='%s.txt' % name, level=logging.DEBUG,
                             filemode='w')
-        self.logger = logging.getLogger('CLIENT')
+        self.logger = logging.getLogger(__name__)
         self.ip = '0.0.0.0'
         self.port = port
         self.name = name
@@ -71,7 +81,34 @@ class Client(QtCore.QObject):
         self.clients = set()
         self.clients.add(self.get_self_client_info())
 
+        self.sources = {}
+
         threading.Thread(target=self.receive_data, daemon=True).start()
+
+        self.ping_time = 10
+
+        def ping_clients():
+            while True:
+                time.sleep(self.ping_time)
+                with threading.Lock():
+                    for client in self.clients:
+                        if client.name == self.name:
+                            continue
+                        self.socket.sendto(b'PNG', client.addr())
+
+        threading.Thread(target=ping_clients, daemon=True).start()
+
+        self.alive_clients = {}
+
+    def delete_dead_clients(self):
+        new_time = time.time()
+        addrs = []
+        for addr, old_time in self.alive_clients.items():
+            if new_time - old_time > self.ping_time:
+                self.handle_deleting(DataContainer(**{'address': addr}))
+                addrs.append(addr)
+        for addr in addrs:
+            del self.alive_clients[addr]
 
     def get_self_client_info(self):
         return ClientInfo(self.name, self.port)
@@ -85,11 +122,15 @@ class Client(QtCore.QObject):
         self.request_clients(address)
 
     def on_receive(self, sock: socket.socket):
-        data, addr = sock.recvfrom(2 ** 16)
+        try:
+            data, addr = sock.recvfrom(2 ** 16)
+        except ConnectionResetError:
+            self.delete_dead_clients()
+            return
         data = data.decode()
         action = data[:3]
         data = data[3:]
-        self.logger.debug('action: %s; addr: %s;, data: %s' %
+        self.logger.debug('action: %s; addr: %s; data: %s' %
                           (action, addr, data))
         dc = DataContainer(**{'address': addr,
                               'action': action,
@@ -104,28 +145,119 @@ class Client(QtCore.QObject):
 
     def on_recv(self, container: DataContainer):
         for case in switch(container.action):
-            if case('CLI'):                             #  New ClientInfo
+            if case('CLI'):  # New ClientInfo
                 self.add_client_info(container)
                 break
-            if case('MSG'):                             #  New message
+            if case('MSG'):  # New message
                 self.recv_msg(container)
                 break
-            if case('NCI'):                             #  New ClientInfos
+            if case('NCI'):  # New ClientInfos
                 self.handle_client_infos(container)
                 break
-            if case('CIN'):                             # ClientInfo need
+            if case('CIN'):  # ClientInfos need
                 self.send_client_infos(container)
                 break
-            if case('DEL'):                             # Delete
+            if case('DEL'):  # Delete
                 self.handle_deleting(container)
+                break
+            if case('PNG'):  # Indicate that client alive (ping analog)
+                self.set_alive(container)
+                break
+            if case('URQ'):  # Upload request
+                self.handle_upload_request(container)
+                break
+            if case('URP'):  # Upload reply
+                break
+            if case('ACP'):  # Accept download
+                self.handle_upload(container)
                 break
             if case():
                 self.logger.debug('unknown action: %s' % container.action)
                 break
 
+    def handle_upload(self, container: DataContainer):
+        def upload():
+            sock = socket.socket()
+            sock.connect((container.address[0], int(container.data)))
+            with open(self.sources[container.address], 'rb') as file:
+                is_end = False
+                while True:
+                    _, can_write, _ = select.select([], [sock], [], 0.01)
+                    if is_end:
+                        break
+                    for conn in can_write:
+                        buf = file.read(2 ** 16)
+                        if buf:
+                            conn.send(buf)
+                        else:
+                            is_end = True
+            del self.sources[container.address]
+
+        threading.Thread(target=upload).start()
+
+    @staticmethod
+    def start_downloading(file_path, port: int):
+        sock = socket.socket()
+        sock.bind(('0.0.0.0', port))
+        sock.listen(1)
+
+        def download():
+            remote_socket, addr = sock.accept()
+            with open(file_path, 'wb') as file:
+                is_end = False
+                while True:
+                    can_read, _, _ = select.select([remote_socket], [], [], 0.01)
+                    if is_end:
+                        break
+                    for conn in can_read:
+                        buf = conn.recv(2 ** 16)
+                        if buf:
+                            file.write(buf)
+                        else:
+                            is_end = True
+
+
+        threading.Thread(target=download).start()
+
+    def handle_upload_request(self, container: DataContainer):
+        name = self.item_by_addr(container.address).name
+        filename, size = container.data.split('\n')
+        self.upload_request.emit(filename, size, name)
+
+    def accept_download(self, path: str, name: str):
+        def find_available_port() -> int:
+            min_port = 30000
+            max_port = 40000
+            if sys.platform == 'linux':
+                command = 'netstat -an | grep tcp | grep %s'
+            else:  # sys.platform == 'win32'
+                command = 'netstat -an | find "TCP" | find "%s"'
+            port = None
+            while port is None:
+                tmp_port = random.randint(min_port, max_port)
+                try:
+                    subprocess.check_output(command % tmp_port, shell=True)
+                except subprocess.CalledProcessError:
+                    port = tmp_port
+            return port
+
+        if sys.platform == 'win32':
+            path = path[1:]
+
+        client = self.item_by_name(name)
+        port = find_available_port()
+        self.start_downloading(path, port)
+        self.socket.sendto(b'ACP' + str(port).encode(), client.addr())
+
+    def set_alive(self, container: DataContainer):
+        self.alive_clients[container.address] = time.time()
+
     def handle_deleting(self, container: DataContainer):
-        self.logger.debug('deleting %s' % self.item(container.address).name)
-        self.clients.discard(self.item(container.address))
+        client_info = self.item_by_addr(container.address)
+        self.logger.debug('deleting %s' % client_info.name)
+        self.client_deleted.emit(client_info.name)
+        with threading.Lock():
+            self.clients.discard(client_info)
 
     def delete_me(self):
         self.logger.debug('delete me')
@@ -135,36 +267,53 @@ class Client(QtCore.QObject):
             self.socket.sendto(b'DEL', ci.addr())
 
     def send_client_infos(self, container: DataContainer):
+        self.logger.debug('clients infos sent to %s' % str(container.address))
         msg = 'NCI' + '\n'.join(x.serialize() for x in self.clients)
         bin_msg = msg.encode()
-        print(bin_msg)
         self.socket.sendto(bin_msg, container.address)
 
-    def handle_client_infos(self, container: DataContainer):
-        print(container.data)
-        for line in container.data.split('\n'):
-            ci = ClientInfo.deserialize(line)
-            if ci.ip == 'localhost':
-                ci.ip = container.address[0]
-            self.clients.add(ci)
-            self.send_client_info(self.get_self_client_info(), ci.addr())
-            # self.socket.sendto(b'CLI' + self.get_self_client_info()
-            #                    .serialize()
-            #                    .encode(),
-            #                    ci.addr())
+    def upload(self, source_path: str, dest_client_name: str):
+        client = self.item_by_name(dest_client_name)
 
-    def item(self, addr: tuple) -> ClientInfo:
+        if sys.platform == 'win32':
+            source_path = source_path[1:]
+
+        if client.addr() in self.sources:
+            self.busy.emit()
+            return
+
+        self.sources[client.addr()] = source_path
+        filename = os.path.basename(source_path)
+        size = os.path.getsize(source_path)
+        self.socket.sendto(b'URQ' + filename.encode() + b'\n' +
+                           str(size).encode(), client.addr())
+
+
+    def handle_client_infos(self, container: DataContainer):
+        for line in container.data.split('\n'):
+            tmp_container = DataContainer(**{'address': container.address,
+                                             'data': line})
+            ci = self.add_client_info(tmp_container)
+            self.send_client_info(self.get_self_client_info(), ci.addr())
+
+    def item_by_addr(self, addr: tuple) -> ClientInfo:
         for ci in self.clients:
             if ci.port == addr[1]:
                 return ci
         return ClientInfo('unknown', 0)
 
+    def item_by_name(self, name: str) -> ClientInfo:
+        for ci in self.clients:
+            if ci.name == name:
+                return ci
+        return ClientInfo('unknown', 0)
+
     def recv_msg(self, container: DataContainer):
         self.logger.debug('new message received')
-        msg = "%s: %s" % (self.item(container.address).name, container.data)
+        msg = "%s: %s" % (self.item_by_addr(container.address).name, container.data)
         self.new_message.emit(msg)
 
-    def send_msg(self, msg: str):
+    def send_msg(self, msg: str, private_list: list):
         self.logger.debug('msg "%s" sent' % msg)
         bin_msg = b'MSG' + msg.encode()
         for client in self.clients:
@@ -172,17 +321,24 @@ class Client(QtCore.QObject):
                 self.new_message.emit("<strong>%s</strong>: %s" % (self.name,
                                                                    msg))
                 continue
-            self.socket.sendto(bin_msg, client.addr())
+
+            if len(private_list) == 0:
+                self.socket.sendto(bin_msg, client.addr())
+            elif client.name in private_list:
+                self.socket.sendto(bin_msg, client.addr())
 
     def send_client_info(self, ci: ClientInfo, addr: tuple):
         self.logger.debug('client info "%s %s %s" sent to %s %s' %
                           (ci.name, ci.ip, ci.port, addr[0], addr[1]))
         self.socket.sendto(b'CLI' + ci.serialize().encode(), addr)
 
-    def add_client_info(self, container: DataContainer):
+    def add_client_info(self, container: DataContainer) -> ClientInfo:
         ci = ClientInfo.deserialize(container.data)
+        self.new_client.emit(ci.name)
         if ci.ip == 'localhost':
             ci.ip = container.address[0]
-        if ci in self.clients:
-            self.clients.discard(ci)
-        self.clients.add(ci)
+        with threading.Lock():
+            if ci in self.clients:
+                self.clients.discard(ci)
+            self.clients.add(ci)
+        return ci
--- gui.py
+++ gui.py
@@ -1,27 +1,102 @@
+import sys
+
 __author__ = 'Галлям'
 
-from PyQt5 import QtCore, QtWidgets
+from PyQt5 import QtCore, QtWidgets, Qt
 from client import Client
 
 
+# noinspection PyUnresolvedReferences
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, name: str, port: int):
+    def initialise_client(self, name, port):
+        if isinstance(port, str):
+            port = int(port)
+        self.client = Client(port, name)
+
+        self.client.upload_request.connect(self.upload_request)
+        self.client.busy.connect(self.busy)
+
+    @staticmethod
+    def busy():
+        information = QtWidgets.QMessageBox()
+        information.setWindowTitle('Information')
+        information.setText('You are uploading file now.\r\n'
+                            'Wait until upload complete.')
+        information.addButton(QtWidgets.QMessageBox.Ok)
+        information.setIcon(QtWidgets.QMessageBox.Information)
+        information.setWindowModality(QtCore.Qt.ApplicationModal)
+        information.exec()
+
+    def upload_request(self, filename: str, size: str, name: str):
+        request_window = QtWidgets.QMessageBox()
+        request_window.setWindowTitle("Download?")
+        request_window.setText("User name: %s\r\n"
+                               "File name: %s\r\n"
+                               "Size: %s\r\n"
+                               "Do you want to continue?" % (name,
+                                                             filename,
+                                                             size))
+        yes_button = request_window.addButton(QtWidgets.QMessageBox.Yes)
+        request_window.addButton(QtWidgets.QMessageBox.No)
+        request_window.setIcon(QtWidgets.QMessageBox.Information)
+        request_window.setWindowModality(QtCore.Qt.ApplicationModal)
+        request_window.exec()
+
+        if request_window.clickedButton() == yes_button:
+            fd = QtWidgets.QFileDialog()
+            dir_url = fd.getExistingDirectoryUrl()
+            self.client.accept_download(dir_url.path() + '/' + filename, name)
+        else:
+            pass
+
+    def __init__(self, name: str=None, port: int=None):
         super().__init__()
+        self.name = name
+        self.port = port
         self.client = None
-        self.initialise(name, port)
 
-    def closeEvent(self, event):
-        self.client.delete_me()
-        event.accept()
+        self.privates = []
 
-    def initialise(self, name: str, port: int):
-        client = Client(port, name)
+        self.setAcceptDrops(True)
 
-        self.client = client
+        if name is None:
+            self.get_name_and_port()
+            pass
+        else:
+            self.main_init(name, port)
 
+    def main_init(self, name: str, port: int):
+        self.initialise_client(name, port)
+        self.create_main_widget()
+        self.initialise()
+        self.showNormal()
+        self.setFocus(QtCore.Qt.ActiveWindowFocusReason)
+
+    def set_name(self, name: str):
+        self.name = name
+
+    def set_port(self, port: int):
+        self.port = port
+
+    def get_name_and_port(self):
+        self.hide()
+        getter = StartWindow()
+        self.g = getter
+
+        def on_click():
+            name = getter.name_line_edit.text()
+            port = int(getter.port_line_edit.text())
+            self.main_init(name, port)
+
+        getter.ok_button.clicked.connect(on_click)
+        getter.ok_button.clicked.connect(getter.close)
+        getter.show()
+
+    def create_main_widget(self):
         central = QtWidgets.QWidget()
+        central.setAcceptDrops(True)
 
-        vbox_layout = QtWidgets.QVBoxLayout(central)
+        main_layout = QtWidgets.QVBoxLayout(central)
 
         msg_layout = QtWidgets.QHBoxLayout()
         send_btn = QtWidgets.QPushButton("&Send")
@@ -31,22 +106,52 @@ class MainWindow(QtWidgets.QMainWindow):
 
         def send_msg():
             message = msg.text()
-            client.send_msg(message)
+            self.client.send_msg(message, self.privates.copy())
+            self.privates.clear()
 
         send_btn.clicked.connect(send_msg)
         send_btn.clicked.connect(msg.clear)
         msg_layout.addWidget(msg)
         msg_layout.addWidget(send_btn)
 
-        vbox_layout.addLayout(msg_layout)
+        grid_layout = QtWidgets.QGridLayout()
+        main_layout.addLayout(msg_layout)
+        main_layout.addLayout(grid_layout)
 
         text = QtWidgets.QTextEdit()
         text.setReadOnly(True)
-        client.new_message.connect(text.append)
-        vbox_layout.addWidget(text)
+        self.client.new_message.connect(text.append)
+        grid_layout.addWidget(text, 0, 0, 0, 1)
+
+        clients_list = ClientsList()
+
+        clients_list.setFixedWidth(74)
+        self.client.new_client.connect(clients_list.addItem)
+
+        def set_private(names: list):
+            self.privates = names.copy()
+
+        clients_list.private_with.connect(set_private)
+        clients_list.upload_file.connect(self.client.upload)
+
+        def del_client(name: str):
+            match = clients_list.findItems(name, QtCore.Qt.MatchExactly)
+            clients_list.takeItem(clients_list.row(match[0]))
+
+        self.client.client_deleted.connect(del_client)
+        grid_layout.addWidget(clients_list, 0, 1)
+
+        clear_button = QtWidgets.QPushButton('&Reset')
+        clear_button.clicked.connect(lambda: clients_list.clearSelection())
+        grid_layout.addWidget(clear_button, 1, 1)
 
         self.setCentralWidget(central)
 
+    def closeEvent(self, event):
+        self.client.delete_me()
+        event.accept()
+
+    def initialise(self):
         main_toolbar = self.addToolBar('Main')
 
         def conn_to():
@@ -68,7 +173,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 ip = ip_line_edit.text()
                 tmp.hide()
                 port = int(port_line_edit.text())
-                client.connect(ip, port)
+                self.client.connect(ip, port)
 
             ok = QtWidgets.QPushButton('OK')
 
@@ -82,46 +187,75 @@ class MainWindow(QtWidgets.QMainWindow):
         connect_action = QtWidgets.QAction('Connect', self)
         connect_action.triggered.connect(conn_to)
         main_toolbar.addAction(connect_action)
-        self.show()
 
 
-def create_main_window(port, name):
-    print(port, name)
-    main = MainWindow(name, port)
-    main.show()
-    return main
+class ClientsList(QtWidgets.QListWidget):
+    wrong_files_count = QtCore.pyqtSignal()
+    private_with = QtCore.pyqtSignal(list)
+    upload_file = QtCore.pyqtSignal(str, str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setDropIndicatorShown(True)
+        self.setDefaultDropAction(QtCore.Qt.CopyAction)
+        self.setDragDropMode(QtWidgets.QAbstractItemView.DropOnly)
+        self.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+
+    def selectionChanged(self, *args, **kwargs):
+        if len(self.selectedItems()) == 0:
+            return
+        self.private_with.emit([item.text() for item in self.selectedItems()])
+
+    def contextMenuEvent(self, event: Qt.QContextMenuEvent):
+
+        index = self.indexAt(event.pos())
+        if index.isValid():
+            menu = QtWidgets.QMenu(self)
+            name = index.data(QtCore.Qt.DisplayRole)
+            upload_action = QtWidgets.QAction('Upload file', menu)
+
+            def upload():
+                w = QtWidgets.QFileDialog()
+                url = w.getOpenFileUrl()[0]
+                if url.path() != '':
+                    self.upload_file.emit(url.path(), name)
+
+            upload_action.triggered.connect(upload)
+            menu.addAction(upload_action)
+            menu.exec(event.globalPos())
+
+
+# noinspection PyUnresolvedReferences
+class StartWindow(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
+        port_line_edit = QtWidgets.QLineEdit()
+        name_line_edit = QtWidgets.QLineEdit()
+
+        self.port_line_edit = port_line_edit
+        self.name_line_edit = name_line_edit
+
+        port_layout = QtWidgets.QFormLayout()
+        port_layout.addRow('Enter port:', port_line_edit)
+
+        name_layout = QtWidgets.QFormLayout()
+        name_layout.addRow('Enter your name:', name_line_edit)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addLayout(port_layout)
+        main_layout.addLayout(name_layout)
+
+        ok = QtWidgets.QPushButton('OK')
+        self.ok_button = ok
+
+        port_line_edit.returnPressed.connect(ok.click)
+        name_line_edit.returnPressed.connect(ok.click)
+
+        main_layout.addWidget(ok)
 
 
 if __name__ == '__main__':
-    main = None
     app = QtWidgets.QApplication([])
-    getter = QtWidgets.QWidget()
-    port_line_edit = QtWidgets.QLineEdit()
-    name_line_edit = QtWidgets.QLineEdit()
-
-    port_layout = QtWidgets.QFormLayout()
-    port_layout.addRow('Enter port:', port_line_edit)
-
-    name_layout = QtWidgets.QFormLayout()
-    name_layout.addRow('Enter your name:', name_line_edit)
-
-    main_layout = QtWidgets.QVBoxLayout(getter)
-    main_layout.addLayout(port_layout)
-    main_layout.addLayout(name_layout)
-
-    def on_click():
-        getter.hide()
-        port = int(port_line_edit.text())
-        name = name_line_edit.text()
-        global main
-        main = create_main_window(port, name)
-
-    ok = QtWidgets.QPushButton('OK')
-
-    port_line_edit.returnPressed.connect(ok.click)
-    name_line_edit.returnPressed.connect(ok.click)
-
-    ok.clicked.connect(on_click)
-    main_layout.addWidget(ok)
-    getter.show()
-    app.exec_()
+    main = MainWindow(*sys.argv[1:])
+    sys.exit(app.exec_())
